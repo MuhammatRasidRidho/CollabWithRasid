0972a1c59fada65935724f3ac9108f6b347f3761
Mas Rasid <rasid517@gmail.com>
2020-05-14 14:49:03 +0700

    Initial Commit

---
+++ CollabWithRasid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollabWithRasid
+//
+//  Created by Muhammat Rasid Ridho on 14/05/20.
+//  Copyright © 2020 Muhammat Rasid Ridho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
